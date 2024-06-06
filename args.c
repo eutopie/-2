4414da47d700556e8d1140603416995e6e917cca
Владимир <animebit818@gmail.com>
2024-06-06 23:43:40 +0700

    args

---
+++ args.c
@@ -0,0 +1,22 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[], char *envp[]) {
+	printf("Колличество параметров переданных программе %d\n", argc);
+	if (argc>1) {
+		for(int i=0 ; i<argc; i++) {
+			printf("Параметр %d: %s\n", i, argv[i]);
+		}
+	}
+	else{
+		int j=0;
+		while(envp[j]!=NULL){
+			printf("Параметр окружения %d: %s\n", j, envp[j]);
+			j++;
+			}
+	}
+	return 0;
+}
